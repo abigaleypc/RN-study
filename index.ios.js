25c80326ffb7cec5d5b9c6ebcca8286b3f84b3c0
Yu Abigale <yupeichun@kingsoft.com>
2017-06-14 17:00:34 +0800

    update

--- index.ios.js
+++ index.ios.js
@@ -3,16 +3,34 @@ import {
   AppRegistry,
   View,
   Text,
-  TouchableHighlight
+  TouchableHighlight,
+  StyleSheet,
+  ScrollView
 } from 'react-native';
 
 class AwesomeProject extends React.Component {
+  _onPressButton() {
+    alert("您点击了此按钮");
+  }
+  _onLongPressButton() {
+    alert("您长按了此按钮");
+
+  }
   render() {
     return (
-      <View>
-        <TouchableHighlight>
-          <Text>Button</Text>
+      <View styles={{paddingTop:20}}>
+        <TouchableHighlight 
+          onPress={this._onPressButton} 
+          onLongPress={this._onLongPressButton}
+          style={styles.button}>
+          <Text style={styles.text}>请控制这个按钮：点击、长按</Text>
         </TouchableHighlight>
+          <ScrollView 
+            style={styles.scrollView}
+            maximumZoomScale={3}>
+            <Text style={styles.text}>scrollView内容</Text>
+
+          </ScrollView>
       </View>
      )
 
@@ -20,3 +38,18 @@ class AwesomeProject extends React.Component {
 }
 
 AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
+
+let styles = StyleSheet.create({
+  button: {
+    padding: 20,
+    backgroundColor: '#333'
+  },
+  text: {
+    color: '#fff'
+  },
+  scrollView: {
+    width: 400,
+    height: 600,
+    backgroundColor: '#333'
+  }
+})
