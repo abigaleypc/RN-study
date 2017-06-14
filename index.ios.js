4d328888eda7a8ad6cbb8a2459e6d0d2cc84fd30
Yu Abigale <yupeichun@kingsoft.com>
2017-06-14 15:25:09 +0800

    Update

--- index.ios.js
+++ index.ios.js
@@ -2,34 +2,19 @@ import React from 'react';
 import {
   AppRegistry,
   View,
-  Text,
-  Button
+  Text
 } from 'react-native';
-import { StackNavigator } from 'react-navigation';
-import ChatScreen from './src/component/ChatScreen';
 
-class HomeScreen extends React.Component {
-  static navigationOptions = {
-    title: 'Welcome',
-  };
+class AwesomeProject extends React.Component {
   render() {
     const { navigate } = this.props.navigation;
     return (
       <View>
         <Text>Hello, Navigation!</Text>
-         <Button
-          onPress={() => navigate('Chat')}
-          title="Chat with Lucy"
-        />
       </View>
      )
 
   }
 }
-
-const AwesomeProject = StackNavigator({
-  Home: { screen: HomeScreen },
-  Chat: { screen: ChatScreen },
-});
 
 AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
