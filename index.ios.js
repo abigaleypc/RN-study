9c92faa2a2d480e8e70714c845eeda35553d5263
Yu Abigale <yupeichun@kingsoft.com>
2017-06-14 15:13:03 +0800

    color style

--- index.ios.js
+++ index.ios.js
@@ -2,34 +2,28 @@ import React from 'react';
 import {
   AppRegistry,
   View,
-  Text,
-  Button
+  Text
 } from 'react-native';
-import { StackNavigator } from 'react-navigation';
-import ChatScreen from './src/component/ChatScreen';
 
-class HomeScreen extends React.Component {
-  static navigationOptions = {
-    title: 'Welcome',
-  };
+class AwesomeProject extends React.Component {
   render() {
-    const { navigate } = this.props.navigation;
     return (
-      <View>
-        <Text>Hello, Navigation!</Text>
-         <Button
-          onPress={() => navigate('Chat')}
-          title="Chat with Lucy"
-        />
+      <View style= {{ paddingTop: 30, backgroundColor:'#000' }}>
+        <Text style= {{ color: '#f0f' }} >'#f0f' (#rgb)</Text>
+        <Text style= {{ color: '#f0fc' }} > '#f0fc'(#rgba)</Text>
+        <Text style= {{ color: '#ff00ff' }}  >'#ff00ff' (#rrggbb)</Text>
+        <Text style= {{ color: '#ff00ff05' }} > '#ff00ff05'(#rrggbbaa)</Text>
+        <Text style= {{ color: 'rgb(255, 255, 255)' }} >'rgb(255, 255, 255)'</Text>
+        <Text style= {{ color: 'rgba(255, 255, 255, 1.0)' }} >'rgba(255, 255, 255, 1.0)'</Text>
+        <Text style= {{ color: 'hsl(360, 100%, 100%)' }} >'hsl(360, 100%, 100%)'</Text>
+        <Text style= {{ color: 'hsla(360, 100%, 100%, 1.0)' }} >'hsla(360, 100%, 100%, 1.0)'</Text>
+        <Text style= {{ color: 'transparent' }} >'transparent' </Text>
+        <Text style= {{ color: 'red' }} > 'red'</Text>
+        <Text style= {{ color: 0xff00ff00 }}> 0xff00ff00 (0xrrggbbaa)</Text>
       </View>
      )
 
   }
 }
-
-const AwesomeProject = StackNavigator({
-  Home: { screen: HomeScreen },
-  Chat: { screen: ChatScreen },
-});
 
 AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
--- readme.md
+++ readme.md
@@ -3,3 +3,9 @@
 #### 使用导航器跳转页面
 
 * 切换分支查看代码 `git checkout react-navigation`
+* [实现过程](https://abigaleypc.github.io/2017/06/14/react-native%E4%BD%BF%E7%94%A8%E5%AF%BC%E8%88%AA%E5%99%A8%E8%B7%B3%E8%BD%AC%E9%A1%B5%E9%9D%A2/)
+
+
+#### 颜色的多种写法
+
+* 切换分支 `git checkout color`
